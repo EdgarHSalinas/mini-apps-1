841daf899ecfe83ab134c940faa60a0247a4749d
EdgarHSalinas <EdgarHSalinas@gmail.com>
2018-05-21 12:24:03 -0700

    Started some implentation pseudo code

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,29 @@
+var state = 'X';
+
+// Start at X and after first player chooses one box. It will toggle to 0
 
 
+const sqOne = document.getElementById('btn1');
+
+sqOne.addEventListener('click', function(event) {
+    // change inner to X
+    // if toggled change to 0
+
+    console.log('sqOne pressed ', sqOne  );
+})
+
+
+// Implementation
+
+  // Check if position has been used 
+     // if in in use alert a warning
+     // don't allow change of player until one box is checked
+
+
+  // After Each play look for 3 in a row
+
+    // Diagnolly
+    // Horizontally 
+    // Vertically
+
+  // If a condition is met Display message 
