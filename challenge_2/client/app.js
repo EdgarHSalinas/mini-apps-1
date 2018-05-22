2426df3f0f3b2e4630f64f058b833783d630d98e
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-05-22 13:55:49 -0700

    setup for package json and notes

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,19 @@
+var data = {'bob':'foo','paul':'dog'};
+var url = 'http://localhost:3000/';
 
+(function($) {
+  function processForm ( e ) {
+   var request = $.ajax({
+      url: url,
+      data: data,
+      type: 'post',
+      dataType : 'json',
 
+    });
+
+    e.preventDefault();
+  }
+  $("form").submit(processForm);
+})(jQuery);
+
+console.log("JQuery Logged")
---
+++ challenge_2/notes.md
@@ -0,0 +1,13 @@
+## CSV Report Generator
+
+### Basic Requirements
+
+Single page client-server app that generates CSV reports from JSON Data
+
+
+- Use Express to serve up index.html
+
+- Client sends JSON to server, receives back CSV-formatted response that displays on the page
+
+- Implement all login on the server without any external libraries 
+
