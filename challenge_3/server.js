b28c364c7fa0109ace8bd3dd59b74f19329d9b09
EdgarHSalinas <EdgarHSalinas@gmail.com>
2018-05-23 15:48:35 -0700

    completed Mocha Chai setup

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 var port = process.env.PORT || 3000;
 // app.get('/', (req, res) => res.send('Checkout'));
 
