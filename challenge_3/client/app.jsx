ae63db81018b36abc2f01bfe974a9186b1ffc654
EdgarHSalinas <EdgarHSalinas@gmail.com>
2018-05-23 12:27:28 -0700

    Complete setup of Env

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,11 @@
+// import React from '../public/react.development.js';
+// import ReactDom from '../public/react-dom.development.js';
+
+console.log('React App JSX');
+ReactDOM.render(
+ <h1>Checkout!</h1>, document.getElementById('app')
+);
+
+
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
 
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
+// app.get('/', (req, res) => res.send('Checkout'));
+
+app.use(express.static('public'));
+
+app.listen(port, () => console.log('Listening on Port 3000'));
 
